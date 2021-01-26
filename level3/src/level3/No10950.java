99e5a857ea53e6a2ccce63e4a1b4741f3775fb90
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-01-26 14:52:58 +0900

    A+B - 3

---
+++ level3/src/level3/No10950.java
@@ -0,0 +1,20 @@
+package level3;
+
+import java.util.Scanner;
+
+public class No10950 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int testCases = scanner.nextInt();
+		
+		for (int i = 0; i < testCases; i++) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			
+			System.out.println(A + B);
+		}
+	}
+
+}
