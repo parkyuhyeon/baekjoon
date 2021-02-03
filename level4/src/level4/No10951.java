7d5d2739e6c8b075df5b288e14081acb8b7db7f8
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-03 19:00:23 +0900

    더하기 사이클

---
+++ level4/src/level4/No10951.java
@@ -0,0 +1,18 @@
+package level4;
+
+import java.util.Scanner;
+
+public class No10951 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while (scanner.hasNext()) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			
+			System.out.println(A + B);
+		}
+	}
+
+}
