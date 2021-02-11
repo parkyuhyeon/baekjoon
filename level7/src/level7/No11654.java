80ce3592058ec60b1edafe076a6da7848001aa16
parkyuhyeon <yuhyeon.dev@gmail.com>
2021-02-11 14:12:46 +0900

    아스키 코드

---
+++ level7/src/level7/No11654.java
@@ -0,0 +1,15 @@
+package level7;
+
+import java.util.Scanner;
+
+public class No11654 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int ascii = scanner.next().charAt(0);
+		
+		System.out.println(ascii);
+	}
+
+}
